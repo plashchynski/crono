2fd061a523470f533384e2ca2299d9bb885901eb
Dzmitry Plashchynski <plashchynski@gmail.com>
2015-03-02 21:55:26 +0200

    Remove rails extension

--- lib/periodicity.rb
+++ lib/periodicity.rb
@@ -5,5 +5,3 @@ require "active_support/all"
 require "periodicity/version.rb"
 require "periodicity/period.rb"
 require "periodicity/config.rb"
-require 'periodicity/extensions/active_job'
-require "periodicity/rails.rb" if defined?(::Rails::Engine)
--- lib/periodicity/extensions/active_job.rb
+++
@@ -1,9 +0,0 @@
-module Periodicity
-  module Extensions
-    module ActiveJob
-      def perform_every(period, *args)
-        Config.instance.schedule += [self, Period.new(period, *args)]
-      end
-    end
-  end
-end
