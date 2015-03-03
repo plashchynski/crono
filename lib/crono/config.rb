fea559c984006d7e0dd4daac371e5f486400d449
Dzmitry Plashchynski <plashchynski@gmail.com>
2015-03-03 19:45:56 +0200

    Add pidfile option

--- lib/crono/config.rb
+++ lib/crono/config.rb
@@ -2,14 +2,17 @@ module Crono
   class Config
     CRONOTAB  = "config/cronotab.rb"
     LOGFILE   = "log/crono.log"
+    PIDFILE   = "tmp/pids/crono.pid"
 
     attr_accessor :cronotab
     attr_accessor :logfile
+    attr_accessor :pidfile
     attr_accessor :daemonize
 
     def initialize
       self.cronotab = CRONOTAB
       self.logfile  = LOGFILE
+      self.pidfile  = PIDFILE
       self.daemonize = false
     end
   end
--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -2,10 +2,11 @@ require "spec_helper"
 
 describe Crono::Config do
   describe "#initialize" do
-    it "should initialize schedule" do
+    it "should initialize with default configuration options" do
       @config = Crono::Config.new
       expect(@config.cronotab).to be Crono::Config::CRONOTAB
       expect(@config.logfile).to be Crono::Config::LOGFILE
+      expect(@config.pidfile).to be Crono::Config::PIDFILE
       expect(@config.daemonize).to be false
     end
   end
