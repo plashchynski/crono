d0b35aaa6e252bff3f7d8f0765ec6c1b4aebbe5e
Dzmitry Plashchynski <plashchynski@gmail.com>
2015-03-06 23:27:02 +0200

    Validate uniqueness of CronoJob job_id

--- lib/crono/orm/active_record/crono_job.rb
+++ lib/crono/orm/active_record/crono_job.rb
@@ -3,6 +3,6 @@ require 'active_record'
 module Crono
   class CronoJob < ActiveRecord::Base
     self.table_name = "crono_jobs"
-    validates :job_id, presence: true
+    validates :job_id, presence: true, uniqueness: true
   end
 end
--- spec/orm/active_record/crono_job_spec.rb
+++ spec/orm/active_record/crono_job_spec.rb
@@ -15,6 +15,14 @@ describe Crono::CronoJob do
   it "should validate presence of job_id" do
     @crono_job = Crono::CronoJob.new()
     expect(@crono_job).not_to be_valid
+    expect(@crono_job.errors.added?(:job_id, :blank)).to be true
+  end
+
+  it "should validate uniqueness of job_id" do
+    Crono::CronoJob.create!(job_id: "TestJob every 2 days")
+    @crono_job = Crono::CronoJob.create(job_id: "TestJob every 2 days")
+    expect(@crono_job).not_to be_valid
+    expect(@crono_job.errors.added?(:job_id, :taken)).to be true
   end
 
   it "should save job_id to DB" do
