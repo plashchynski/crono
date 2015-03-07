c0b25b2a7caa572d17c2c03ae8c4cec36579dee8
Dzmitry Plashchynski <plashchynski@gmail.com>
2015-03-07 18:50:34 +0200

    Replace the file DB with shared cached memory DB

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,3 @@
-TMP_DB_FILE = "tmp/test_db.sqlite3"
-
 require 'bundler/setup'
 Bundler.setup
 
@@ -8,9 +6,7 @@ require 'byebug'
 require 'crono'
 require 'generators/crono/install/templates/migrations/create_crono_jobs.rb'
 
-FileUtils.rm(TMP_DB_FILE) if File.exist?(TMP_DB_FILE)
-
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: TMP_DB_FILE)
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: "file::memory:?cache=shared")
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 CreateCronoJobs.up
 
