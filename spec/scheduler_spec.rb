98f058767f3dfd71ebe54e600f2eb5dd123fbcb5
Dzmitry Plashchynski <plashchynski@gmail.com>
2015-03-07 16:06:40 +0200

    Fix spec

--- spec/scheduler_spec.rb
+++ spec/scheduler_spec.rb
@@ -8,9 +8,9 @@ describe Crono::Scheduler do
   before(:each) do
     @scheduler = Crono::Scheduler.new
     @jobs = [
-      Crono::Period.new(3.day, at: "18:55"),
-      Crono::Period.new(1.day, at: "15:30"),
-      Crono::Period.new(7.day, at: "06:05")
+      Crono::Period.new(3.day, at: 10.minutes.from_now.strftime("%H:%M")),
+      Crono::Period.new(1.day, at: 20.minutes.from_now.strftime("%H:%M")),
+      Crono::Period.new(7.day, at: 40.minutes.from_now.strftime("%H:%M"))
     ].map { |period| Crono::Job.new(TestJob, period) }
     @scheduler.jobs = @jobs
   end
@@ -25,7 +25,7 @@ describe Crono::Scheduler do
 
   describe "#next" do
     it "should return next job in schedule" do
-      expect(@scheduler.next).to be @jobs[2]
+      expect(@scheduler.next).to be @jobs[0]
     end
   end
 end
