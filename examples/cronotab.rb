84717493a8076b90eeaa6943f10c6d79a2a23cf2
Dzmitry Plashchynski <plashchynski@gmail.com>
2015-03-07 03:49:29 +0200

    Simplify text in examples

--- examples/cronotab.rb
+++ examples/cronotab.rb
@@ -1,15 +1,15 @@
-# cronotab.rb — Crono configuration example file
+# cronotab.rb — Crono configuration file
 #
-# Here you can specify periodic jobs and their schedule.
-# You can specify a periodic job as a ActiveJob class in `app/jobs/`
-# Actually you can use any class. The only requirement is that
-# the class should implement a method `perform` without arguments.
+# Here you can specify periodic jobs and schedule.
+# You can use ActiveJob's jobs from `app/jobs/`
+# You can use any class. The only requirement is that
+# class should have a method `perform` without arguments.
 #
-
 class TestJob
   def perform
     puts "Test!"
   end
 end
 
-Crono.perform(TestJob).every 5.second
+Crono.perform(TestJob).every 2.days, at: "15:30"
+
