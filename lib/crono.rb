eedff96d39dce9d71f4cc6cfe4ad7498c7fcc5b3
Dzmitry Plashchynski <plashchynski@gmail.com>
2015-03-09 00:24:58 +0200

    Autoload Crono::Web

--- lib/crono.rb
+++ lib/crono.rb
@@ -2,11 +2,13 @@ module Crono
 end
 
 require "active_support/all"
-require "crono/version.rb"
-require "crono/logging.rb"
-require "crono/period.rb"
-require "crono/job.rb"
-require "crono/scheduler.rb"
-require "crono/config.rb"
-require "crono/performer_proxy.rb"
-require "crono/orm/active_record/crono_job.rb"
+require "crono/version"
+require "crono/logging"
+require "crono/period"
+require "crono/job"
+require "crono/scheduler"
+require "crono/config"
+require "crono/performer_proxy"
+require "crono/orm/active_record/crono_job"
+
+Crono.autoload :Web, "crono/web"
