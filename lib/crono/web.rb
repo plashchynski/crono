b95c480a8feb2ce30f4367435a70ad7b00c43a56
Dzmitry Plashchynski <plashchynski@gmail.com>
2015-03-08 22:25:55 +0200

    Add background for web

--- lib/crono/web.rb
+++ lib/crono/web.rb
@@ -3,9 +3,6 @@ require 'sinatra/base'
 
 module Crono
   class Web < Sinatra::Base
-    def app
-      Sinatra::Application
-      end
     set :root, File.expand_path(File.dirname(__FILE__) + "/../../web")
     set :public_folder, Proc.new { "#{root}/assets" }
     set :views, Proc.new { "#{root}/views" }
