6859d1f09a4b8c76bd7d45e635e9c5bf696bc18d
Dzmitry Plashchynski <plashchynski@gmail.com>
2015-03-10 23:37:15 +0200

    Add specs to test error marks

--- spec/web_spec.rb
+++ spec/web_spec.rb
@@ -19,6 +19,12 @@ describe Crono::Web do
       expect(last_response).to be_ok
       expect(last_response.body).to include @test_job_id
     end
+
+    it "should show a error mark when a job is unhealthy" do
+      @test_job.update(healthy: false)
+      get '/'
+      expect(last_response.body).to include "Error"
+    end
   end
 
   describe "/job/:id" do
@@ -27,6 +33,12 @@ describe Crono::Web do
       expect(last_response).to be_ok
       expect(last_response.body).to include @test_job_id
       expect(last_response.body).to include @test_job_log
+    end
+
+    it "should show a message about the unhealthy job" do
+      @test_job.update(healthy: false)
+      get "/job/#{@test_job.id}"
+      expect(last_response.body).to include "An error occurs during the last execution of this job"
     end
   end
 end
